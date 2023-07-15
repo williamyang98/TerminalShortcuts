b7385303cbbf5c69cf92740c6075ea75771daa63
William Yang <williamyang2017@gmail.com>
2023-07-16 00:50:33 +1000

    Fixed script

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
-curl -fLo ~/.vim/autoload/plug.vim--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp init.vim ~/.vimrc
