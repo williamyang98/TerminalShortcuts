a73359134400afdfc5e1a924412ab5bbdd3d0b09
William Yang <williamyang2017@gmail.com>
2023-07-16 01:23:34 +1000

    Fixed incorrect copy of folder

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,5 +2,6 @@
 set -x
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir -p ~/.config/nvim/
-cp -R ./config-files/ ~/.config/nvim/
+cd ./config-files/
+cp -R . ~/.config/nvim/
 
